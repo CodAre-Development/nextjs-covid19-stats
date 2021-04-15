d7795fdc94f348c90a2afb6749aa7d384af22d1c
Yusuf <66310246+xnamespace@users.noreply.github.com>
2021-04-15 16:53:12 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,11 @@ export default function Index() {
     return <div>
         <Head>
             <title>Global Status</title>
-            
+
+            <meta name={"description"} content={"Covid-19 Global Status"} />
             <link rel="icon" href="https://www.thecompassforsbc.org/sites/default/files/COVID-19%20icon%202FINAL.png"/>
         </Head>
+
         <MainComponent />
     </div>
 }
